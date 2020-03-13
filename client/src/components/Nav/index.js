ae718647ce76d55b5af19e98c581c63dcee61aa9
hamzaaaabdikarim33 <55111034+hamzaaaabdikarim33@users.noreply.github.com>
2020-03-12 23:12:54 -0500

    last commit

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -6,10 +6,33 @@ function Nav() {
   return (
     <nav id="nav" className="navbar navbar-expand-lg">
       <a className="navbar-brand" href="/">
-        Google Books
+        Hamza Google Books
       </a>
-      <Link id="search" to="/" className={window.location.pathname === "/" || window.location.pathname === "/" ? "nav-link active" : "nav-link" } > Search </Link>
-      <Link id="saved" to="/saved" className={window.location.pathname === "/saved" || window.location.pathname === "/saved" ? "nav-link active" : "nav-link" } > Saved </Link>
+      <Link
+        id="search"
+        to="/"
+        className={
+          window.location.pathname === "/" || window.location.pathname === "/"
+            ? "nav-link active"
+            : "nav-link"
+        }
+      >
+        {" "}
+        Search{" "}
+      </Link>
+      <Link
+        id="saved"
+        to="/saved"
+        className={
+          window.location.pathname === "/saved" ||
+          window.location.pathname === "/saved"
+            ? "nav-link active"
+            : "nav-link"
+        }
+      >
+        {" "}
+        Saved{" "}
+      </Link>
     </nav>
   );
 }
